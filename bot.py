f75b69ffd486e10bf0f683124840981482e4cd31
anton <anton39reg@mail.ru>
2019-04-28 18:30:10 +0300

    first_step

---
+++ bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+
+bot = telebot.TeleBot('889866095:AAEK8yduZz8nWky-bJW_FkVVuXeAkBL5oNk')
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Hi":
+        bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+
+    elif message.text == "How are you?" or message.text == "How are u?":
+        bot.send_message(message.from_user.id, "I'm fine, thanks. And you?")
+
+    else:
+        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
