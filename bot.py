55dce4823fb396c18ae0daef52cd9cc128de9a01
anton <anton39reg@mail.ru>
2019-05-03 16:42:34 +0300

    start

--- bot.py
+++ bot.py
@@ -5,15 +5,15 @@ bot = telebot.TeleBot('889866095:AAEK8yduZz8nWky-bJW_FkVVuXeAkBL5oNk')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text == "Hi":
-        bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет! Напиши /help, чтобы узнать, что я умею")
 
-    elif message.text == "How are you?" or message.text == "How are u?":
-        bot.send_message(message.from_user.id, "I'm fine, thanks. And you?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Извини, но я ещё ничего не умею))")
 
     else:
-        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
+        bot.send_message(message.from_user.id, "Я тебя не понимаю, напиши /help")
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True, timeout=0)
+    bot.polling(none_stop=True, timeout=1)
