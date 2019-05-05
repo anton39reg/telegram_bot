12f5400a506a72f657869d4b0f0587d66585b133
anton <anton39reg@mail.ru>
2019-05-05 10:48:05 +0300

    tmp

--- bot.py
+++ bot.py
@@ -1,27 +1,23 @@
 import telebot
 
 
-class Bot:
-    bot = telebot.TeleBot('889866095:AAEK8yduZz8nWky-bJW_FkVVuXeAkBL5oNk')
+bot = telebot.TeleBot('889866095:AAEK8yduZz8nWky-bJW_FkVVuXeAkBL5oNk')
 
-    @bot.message_handler(content_types=["text"])
-    def handle_text(self, message):
-        if message.text == "Привет":
-            bot.send_message(message.from_user.id, "Привет! Напиши /help, чтобы узнать, что я умею")
 
-        elif message.text == "/help":
-            bot.send_message(message.from_user.id, "Извини, но я ещё ничего не умею))")
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет! Напиши /help, чтобы узнать, что я умею")
 
-        else:
-            bot.send_message(message.from_user.id, "Я тебя не понимаю, напиши /help")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Извини, но я ещё ничего не умею))")
 
-    def run(self):
-        try:
-            self.bot.polling(none_stop=True, timeout=1)
-        except Exception:
-            pass
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю, напиши /help")
 
 
 if __name__ == '__main__':
-    bot = Bot()
-    bot.run()
+    try:
+        bot.polling(none_stop=True, timeout=1)
+    except Exception:
+        pass
